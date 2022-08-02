b62c943f5d397c9c2825beb38bf512cde977a6f1
Jeison Rodriguez <Jeiison.rodriguez7@gmail.com>
2022-08-01 22:48:38 -0500

    first commit

---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import NavBar from "../NavBar";
+import styles from "./MainLayout.module.css";
+
+const MainLayout = ({children}) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contact - Next Yeison App</title>
+        <meta name="description" content="About Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar/>
+
+
+      <main className={styles.main}>
+      {children}
+      </main>
+    </div>
+  )
+}
+
+export default MainLayout
